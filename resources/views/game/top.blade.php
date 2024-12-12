3f77719894f7ec8f1ddeb6e824da0377e85a602d
haramoeno <moe.piece1201@ezweb.ne.jp>
2024-12-12 15:06:33 +0000

    12/12 top画面レイアウト作成

--- resources/views/game/top.blade.php
+++ resources/views/game/top.blade.php
@@ -1,11 +1,16 @@
-{{-- layouts/admin.blade.phpを読み込む --}}
-@extends('layouts.admin')
-
-
-@section('title', 'ゲームのtop画面')
-
-@section('content')
-    <div class="login">
-        <div class="row">
-            <div class="col-md-8 mx-auto">
-            <h2>ログイン</h2>
+<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>TOPページ</title>
+        <link href="{{ secure_asset('css/top.css') }}" rel="stylesheet">
+    </head>
+    　  <body>
+    　       <div class="image">
+    　       <img src ="/storage/dot-picture-game-style2.jpg">
+    　       <input type="button" class="login" style="text-decoration:none;" onclick="location.href='login'" value="ログイン">
+    　      
+    　       <input type="button" class="register" style="text-decoration:none;" onclick="location.href='register'" value="新規登録">
+    　       </div>
+    　      
+    　  </body>
