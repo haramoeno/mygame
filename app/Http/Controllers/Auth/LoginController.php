fff4d12e6f8de9aa81c30d48f4e3a10b6916b62d
haramoeno <moe.piece1201@ezweb.ne.jp>
2025-01-07 13:48:32 +0000

    01/07 リダイレクト先変更

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'game/search';
+    protected $redirectTo = 'game/';
 
     /**
      * Create a new controller instance.
