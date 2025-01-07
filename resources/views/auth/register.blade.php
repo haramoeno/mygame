224846327cd0999c566a72db82d0068c6b224e31
haramoeno <moe.piece1201@ezweb.ne.jp>
2025-01-07 14:13:43 +0000

    01/07 register.blade.php 修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,14 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
+
+<link href="{{ secure_asset('css/register.css') }}" rel="stylesheet">
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">{{ __('新規登録') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
